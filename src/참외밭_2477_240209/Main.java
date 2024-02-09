8d723cabaf0cfc9ccffba30c911618ba7c48d08b
yngbao97 <yukyj0418@gmail.com>
2024-02-10 00:30:58 +0900

    참외밭 완료

--- src/참외밭_2477_240209/Main.java
+++ src/참외밭_2477_240209/Main.java
@@ -11,26 +11,30 @@ public class Main {
 		int K = sc.nextInt();		// 면적당 참외 개수
 		int[] sides = new int[6];
 		
-		int fullIdx_1 = 0;
-		int fullIdx_2 = 0;
+		int firstIdx = 0;
+		int secondIdx = 0;
+		int idxGap = 0;
+		
 		for (int i = 0; i < 6; i++) {
 			sc.nextInt();
 			int side = sc.nextInt();
 			sides[i] = side;
-			if (sides[i] > sides[fullIdx_1]) fullIdx_1 = i;
+			if (sides[i] > sides[firstIdx]) firstIdx = i;
 		}
 		
-		if (sides[(fullIdx_1 -1+6)%6] > sides[(fullIdx_1 +1)%6]) {
-			fullIdx_2 = (fullIdx_1 - 1+6) % 6;
-		} else fullIdx_2 = (fullIdx_1 + 1) % 6;
+		if (sides[(firstIdx-1+6)%6] > sides[(firstIdx+1)%6]) {
+			secondIdx = (firstIdx-1+6)%6;
+			idxGap = 1;
+		}
+		else {
+			secondIdx = (firstIdx+1)%6;
+			idxGap = -1;
+		}
 		
-		int idxGap = fullIdx_1 - fullIdx_2;
-		if (idxGap == 5 || idxGap == -5) idxGap = 1;
+		int insideIdx_1 = (firstIdx+idxGap+idxGap+6) % 6;
+		int insideIdx_2 = (insideIdx_1+idxGap+6) % 6;
 		
-		int insideIdx_1 = ((fullIdx_1 + (idxGap*2))+6) % 6;
-		int insideIdx_2 = ((insideIdx_1 + idxGap)+6) % 6;
-		
-		int large = (sides[fullIdx_1] * sides[fullIdx_2])
+		int large = (sides[firstIdx] * sides[secondIdx]) 
 				- (sides[insideIdx_1] * sides[insideIdx_2]);
 		
 		System.out.println(K*large);

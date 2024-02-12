7b863928a3a898a1abde5a5c625373f7667bbd66
yngbao97 <yukyj0418@gmail.com>
2024-02-12 17:32:31 +0900

    2292_벌집 풀이 완료

---
+++ src/벌집_2292_240212/Main.java
@@ -0,0 +1,27 @@
+package 벌집_2292_240212;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int num = 1;
+		int cnt = 1;
+		int answer = 1;
+		
+		while(N != 1) {
+			if (N > num && N <= num+(6*cnt)) {
+				answer = cnt+1;
+				break;
+			} else num += 6*cnt++;
+		}
+		
+		System.out.println(answer);
+		sc.close();
+	}
+}

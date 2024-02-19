dfac1aaeccba4a3246efef593afed5365629940c
yngbao97 <yukyj0418@gmail.com>
2024-02-19 17:56:34 +0900

    직사각형을 만드는방법 푸는 중

--- 240204/src/BOJ_8320_직사각형을만드는방법/Main.java
+++ 240204/src/BOJ_8320_직사각형을만드는방법/Main.java
@@ -1,5 +1,19 @@
 package BOJ_8320_직사각형을만드는방법;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int root = (int)Math.sqrt(num);
+		
+		for (int i = root; i > 0; i--) {
+			
+		}
+
+		sc.close();
+	}
 }

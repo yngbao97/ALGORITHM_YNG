960250c9a586831956dfed4ff6ebd9bf63f4306f
yngbao97 <yukyj0418@gmail.com>
2024-02-20 08:18:50 +0900

    방배정 주석 추가 및 오류교정 풀이 완료

--- 240204/src/BOJ_1037_오류교정/Main.java
+++ 240204/src/BOJ_1037_오류교정/Main.java
@@ -1,5 +1,67 @@
 package BOJ_1037_오류교정;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();							// 배열 사이즈
+		int[][] message = new int[N][N];				// 메시지 정보 변수
+		int[][] cnt = new int[2][N];					// 각 행렬의 참 개수
+		
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				message[r][c] = sc.nextInt();			// 메시지 입력하면서
+				cnt[0][r] += message[r][c];				// 행 참 개수 세기
+			}
+		}
+		
+		for (int c = 0; c < N; c++) {
+			for (int r = 0; r < N; r++) {
+				cnt[1][c] += message[r][c];				// 열 참 개수 세기
+			}
+		}
+		
+		int answer = 0;									// 답을 1,2,3으로 구분
+		int rowIdx = -1;								// 참이 홀수인 행 인덱스
+		int colIdx = -1;								// 참이 홀수인 열 인덱스
+		
+		out: for (int r = 0; r < 2; r++) {
+			for (int c = 0; c < N; c++) {
+				
+				if (cnt[r][c] % 2 != 0) {				// 홀수인걸 찾아
+					if (r == 0 && rowIdx != -1) {		// 행인데 이미 홀수인 행이 있으면
+						answer = 3;						// 수정도 불가하므로 3, 순회 종료
+						break out;
+						
+					} else if (r == 0) {				// 홀수인 행이 처음이면
+						rowIdx = c;						// 행인덱스 값 할당
+						
+					} else if (r == 1 && colIdx != -1) {	// 열인데 이미 홀수인 열이 있으면
+						answer = 3;							// 수정도 불가하므로 3, 순회 종료
+						break out;
+						
+					} else if (r == 1) {				// 홀수인 열이 처음이면
+						colIdx = c;						// 열인덱스 값 할당
+					}
+				}
+			}
+		}
+		
+		if (rowIdx == -1 && colIdx == -1) answer = 1;			// 홀수인 행렬이 없으면 1
+		else if (rowIdx != -1 && colIdx != -1) answer = 2;		// 홀수인 행렬이 각각 1개면 2
+		else answer = 3;										// 나머지는 다 3
+		
+		if (answer == 1) {										// 상태에 따른 출력
+			System.out.println("OK");
+		} else if (answer == 2) {
+			System.out.printf("Change bit (%d,%d)\n", rowIdx+1, colIdx+1);
+		} else if(answer == 3) {
+			System.out.println("Corrupt");
+		}
+		
+		sc.close();												// 문단속
+	}
 }
--- 240204/src/BOJ_13300_방배정/Main.java
+++ 240204/src/BOJ_13300_방배정/Main.java
@@ -7,24 +7,25 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		int max = sc.nextInt();
-		int[][] students = new int [2][7];
+		int N = sc.nextInt();							// 총 학생 수
+		int max = sc.nextInt();							// 한 방의 최대 인원수
+		int[][] students = new int [2][7];				// 성별에 따른 학년별 인원수
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {					// 입력
 			students[sc.nextInt()][sc.nextInt()]++;
 		}
 		
-		int answer = 0;
+		int answer = 0;									// 필요한 방의 개수
+		
 		for (int i = 0; i < 2; i++) {
 			for (int j = 1; j < 7; j++) {
-				if (students[i][j] % max == 0) {
-					answer += students[i][j] / max;
-				} else answer += (students[i][j] / max) + 1;
+				if (students[i][j] % max == 0) {				// 학생수가 방 최대인원 수로 나누어 떨어지면
+					answer += students[i][j] / max;				// 그 몫을 답에 합함
+				} else answer += (students[i][j] / max) + 1;	// 나머지가 있으면 몫에 +1하여 답에 합함
 			}
 		}
 		
-		System.out.println(answer);
-		sc.close();
+		System.out.println(answer);								// 출력
+		sc.close();												// 문단속
 	}
 }

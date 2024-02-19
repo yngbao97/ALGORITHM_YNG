c0534d6dd3d156b7dfce1f8b805e4ad4371383e2
yngbao97 <yukyj0418@gmail.com>
2024-02-20 07:31:11 +0900

    방배정 풀이 완료

--- 240204/src/BOJ_13300_방배정/Main.java
+++ 240204/src/BOJ_13300_방배정/Main.java
@@ -1,5 +1,30 @@
 package BOJ_13300_방배정;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int max = sc.nextInt();
+		int[][] students = new int [2][7];
+		
+		for (int i = 0; i < N; i++) {
+			students[sc.nextInt()][sc.nextInt()]++;
+		}
+		
+		int answer = 0;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j < 7; j++) {
+				if (students[i][j] % max == 0) {
+					answer += students[i][j] / max;
+				} else answer += (students[i][j] / max) + 1;
+			}
+		}
+		
+		System.out.println(answer);
+		sc.close();
+	}
 }

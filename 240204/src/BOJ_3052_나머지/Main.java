3cfd1705e8a03f7c3b44295090d3d72422c0e501
yngbao97 <yukyj0418@gmail.com>
2024-02-19 17:52:23 +0900

    나머지 풀이 업로드

--- 240204/src/BOJ_3052_나머지/Main.java
+++ 240204/src/BOJ_3052_나머지/Main.java
@@ -1,5 +1,20 @@
 package BOJ_3052_나머지;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int[] cnt = new int[42];						// 나머지가 될 수 있는 수들의 카운트 배열
+		int answer = 0;									// 나머지가 서로 다른 수의 개수
+		
+		for(int i = 0; i < 10; i++) {
+			if(cnt[sc.nextInt() % 42]++ == 0) answer++;	// 나머지 카운트에 처음 추가되는 값이면 답 +1
+		}
+		
+		System.out.println(answer);						// 출력
+		sc.close();										// 문단속
+	}
 }

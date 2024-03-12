035470ba1bddbc4087624e5668518fe27bbbde26
yngbao97 <yukyj0418@gmail.com>
2024-03-13 00:54:55 +0900

    체스판다시칠하기, 단어정렬 풀이

--- 240302/src/boj_1018_체스판다시칠하기/Main.java
+++ 240302/src/boj_1018_체스판다시칠하기/Main.java
@@ -1,5 +1,50 @@
 package boj_1018_체스판다시칠하기;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String[] input = sc.nextLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+		char[][] board = new char[N][M];
+		char[][] example = new char[8][8];
+		
+		for (int r = 0; r < N; r++) {
+			board[r] = sc.nextLine().toCharArray();
+		}
+		
+		String a = "WBWBWBWB";
+		String b = "BWBWBWBW";
+		for (int r = 0; r < 8; r++) {
+			if (r%2 == 0) {
+				example[r] = a.toCharArray();
+			} else {
+				example[r] = b.toCharArray();
+			}
+		}
+		
+		int answer = Integer.MAX_VALUE;
+		
+		for (int r = 0; r <= N-8; r++) {
+			for (int c = 0; c <= M-8; c++) {
+				
+				int cnt = 0;
+				for (int i = 0; i < 8; i++) {
+					for (int j = 0; j < 8; j++) {
+						if (board[r+i][c+j] != example[i][j]) cnt++;
+					}
+				}
+				
+				if (cnt > 32) cnt = 64 - cnt;
+				if (cnt < answer) answer = cnt;
+			}
+		}
+		
+		System.out.println(answer);
+		sc.close();
+	}
 }
--- 240302/src/boj_1181_단어정렬/Main.java
+++ 240302/src/boj_1181_단어정렬/Main.java
@@ -1,5 +1,36 @@
 package boj_1181_단어정렬;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = Integer.parseInt(sc.nextLine());
+		List<String> words = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			String input = sc.nextLine();
+			if (!words.contains(input)) {
+				words.add(input);
+			}
+		}
+		
+		Collections.sort(words, (o1, o2) -> {
+			if (o1.length() == o2.length()) {
+				return o1.compareTo(o2);
+			}
+			return o1.length() - o2.length();
+		});
+		
+		for (String word : words) {
+			System.out.println(word);
+		}
+		
+		sc.close();
+	}
 }

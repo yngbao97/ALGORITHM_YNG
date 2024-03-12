2dedc4bab5765d3f16b1a6239f39610d5e4425a2
yngbao97 <yukyj0418@gmail.com>
2024-03-13 01:02:04 +0900

    팰린드롬수 풀이 업로드

--- 240302/src/boj_1259_팰린드롬수/Main.java
+++ 240302/src/boj_1259_팰린드롬수/Main.java
@@ -1,5 +1,27 @@
 package boj_1259_팰린드롬수;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			String answer = "yes";
+			String input = sc.nextLine();
+			if (input.equals("0")) break;
+			
+			char[] number = input.toCharArray();
+			for (int i = 0, j = input.length()-1; i < input.length()/2; i++, j--) {
+				if (number[i] != number[j]) {
+					answer = "no";
+					break;
+				}
+			}
+			
+			System.out.println(answer);
+		}
+		sc.close();
+	}
 }

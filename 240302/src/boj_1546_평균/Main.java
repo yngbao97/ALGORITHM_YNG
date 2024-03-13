8bf13de0b74c26a3b61926d072d0242e35013f50
yngbao97 <yukyj0418@gmail.com>
2024-03-13 16:43:30 +0900

    백준 평균 풀이 업로드

--- 240302/src/boj_1546_평균/Main.java
+++ 240302/src/boj_1546_평균/Main.java
@@ -1,5 +1,28 @@
 package boj_1546_평균;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double N = sc.nextInt();
+		double[] scores = new double[(int)N];
+		double max = Integer.MIN_VALUE;
+		double sum = 0;
+		
+		for (int i = 0; i < N; i++) {
+			scores[i] = sc.nextInt();
+			if (scores[i] > max) max = scores[i];
+		}
+		
+		for (int i = 0; i < N; i++) {
+			scores[i] = scores[i]/max*100;
+			sum += scores[i];
+		}
+		
+		System.out.println(sum/N);
+		sc.close();
+	}
 }

8dd38903451d83eeedba99aef3bf7417f7007930
yngbao97 <yukyj0418@gmail.com>
2024-03-13 20:54:04 +0900

    스택수열 풀이

--- 240302/src/boj_1874_스택수열/Main.java
+++ 240302/src/boj_1874_스택수열/Main.java
@@ -1,5 +1,54 @@
 package boj_1874_스택수열;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] nums = new int[N];
+		boolean possible = true;
+		
+		for (int i = 0; i < N; i++) {
+			nums[i] = sc.nextInt();
+		}
+		
+		Stack<Integer> stack = new Stack<>();
+		List<Character> answer = new ArrayList<>();
+		int idx = 0;
+		int num = 1;
+		
+		while (idx < N) {
+			
+			if (stack.isEmpty() || stack.peek() < nums[idx]) {
+				stack.push(num++);
+				answer.add('+');
+				
+			} else if (stack.peek() == nums[idx]) {
+				stack.pop();
+				idx++;
+				answer.add('-');
+				
+			} else if (stack.peek() > nums[idx]) {
+				possible = false;
+				break;
+			}
+		}
+		
+		if (possible) {
+			for (char c : answer) {
+				System.out.println(c);
+			}
+			
+		} else {
+			System.out.println("NO");
+		}
+		
+		sc.close();
+	}
 }

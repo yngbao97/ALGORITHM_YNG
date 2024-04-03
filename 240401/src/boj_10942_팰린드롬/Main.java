30056717f15c35e634a67655d409fbcd83055938
yngbao97 <yukyj0418@gmail.com>
2024-04-03 21:54:36 +0900

    마법사상어와파이어볼 풀이 완료

---
+++ 240401/src/boj_10942_팰린드롬/Main.java
@@ -0,0 +1,10 @@
+package boj_10942_팰린드롬;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- 240401/src/boj_20056_마법사상어와파이어볼/Main.java
+++ 240401/src/boj_20056_마법사상어와파이어볼/Main.java
@@ -1,5 +1,7 @@
 package boj_20056_마법사상어와파이어볼;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,8 +11,8 @@ public class Main {
 	static int N;
 	static int M;
 	static int K;
-	static int[][] map;
-	static int[][] balls;
+	static List<int[]>[][] map;
+	static boolean[] moved;
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -19,8 +21,13 @@ public class Main {
 		M = sc.nextInt();		// 파이어볼 개수
 		K = sc.nextInt();		// 명령 횟수
 		
-		map = new int[N][N];	// 각 칸에 파이어볼이 몇개 있는지
-		balls = new int[M][5];	// 각 파이어볼의 위치(r, c)와 질량(m), 속력(s), 방향(d)
+		map = new ArrayList[N][N];	// 각 칸에 있는 파이어볼 정보
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				map[i][j] = new ArrayList<>();
+			}
+		}
 		
 		for (int i = 0; i < M; i++) {
 			int r = sc.nextInt()-1;
@@ -28,8 +35,8 @@ public class Main {
 			int m = sc.nextInt();
 			int s = sc.nextInt();
 			int d = sc.nextInt();
-			map[r][c]++;
-			balls[i] = new int[] {r, c, m, s, d};
+			
+			map[r][c].add(new int[] {r, c, m, s, d});
 		}
 		
 		for (int i = 0; i < K; i++) {
@@ -37,32 +44,99 @@ public class Main {
 			bomb();
 		}
 		
+		int answer = cal();
+		System.out.println(answer);
 		sc.close();
 	}
 	
+	private static int cal() {
+		int sum = 0;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] != null && map[i][j].size() > 0) {
+					for (int[] balls : map[i][j]) {
+						sum += balls[2];
+					}
+				}
+			}
+		}
+		return sum;
+	}
+
 	private static void bomb() {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j] > 1) {
+				if (map[i][j].size() > 1) {
+					int cnt = 0;
+					int m = 0;
+					int s = 0;
+					for (int[] ball : map[i][j]) {
+						m += ball[2];
+						s += ball[3];
+						if (ball[4] % 2 != 0) cnt++;
+					}
+					m /= 5;
+					s /= map[i][j].size();
+					boolean allSame = false;
+					if (cnt == map[i][j].size() || cnt == 0) allSame = true;
+
+					map[i][j].clear();
 					
+					if (m != 0) {
+						
+						if (allSame) {
+							for (int k = 0; k <= 6; k+=2) {
+								map[i][j].add(new int[] {i, j, m, s, k});
+							}
+						} else {
+							for (int k = 1; k <= 7; k+=2) {
+								map[i][j].add(new int[] {i, j, m, s, k});
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private static void move() {
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] != null && map[i][j].size() > 0) {
+					
+					for (int[] balls : map[i][j]) {
+						int d = balls[4];
+						int s = balls[3];
+						
+						balls[0] = (i + (dr[d] * (s%N)) + N) % N;
+						balls[1] = (j + (dc[d] * (s%N)) + N) % N;
+						
+						// 아직 진짜 옮기진 않고 좌표정보만 갱신해줌
+						// 여기서 옮겨두면 다른 칸 돌때 또 옮길수도 있음
+					}
 				}
 			}
 		}
 		
-	}
-
-	private static void move() {
-		for (int i = 0; i < M; i++) {
-			int r = balls[i][0];
-			int c = balls[i][1];
-			int d = balls[i][4];
-			int s = balls[i][3];
-			balls[i][0] = (r + (dr[d] * s) + N) / N;
-			balls[i][1] = (c + (dc[d] * s) + N) / N;
-			
-			map[r][c]--;
-			map[balls[i][0]][balls[i][1]]++;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] != null && map[i][j].size() > 0) {
+					
+					/// 거꾸로 돌아야 삭제시 인덱스 에러가 안남
+					for (int k = map[i][j].size()-1; k >= 0; k--) {
+						
+						int r = map[i][j].get(k)[0];
+						int c = map[i][j].get(k)[1];
+						
+						if (r != i || c != j) {
+							map[r][c].add(map[i][j].get(k));
+							map[i][j].remove(k);
+						}
+					}
+				}
+			}
 		}
 	}
-
 }

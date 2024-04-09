2eaa643258b33cbf274c92026ea8181e308a26c4
yngbao97 <yukyj0418@gmail.com>
2024-04-09 17:43:54 +0900

    동전 1 풀이

--- 240402/src/boj_2293_동전1/Main.java
+++ 240402/src/boj_2293_동전1/Main.java
@@ -1,5 +1,31 @@
 package boj_2293_동전1;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int[] coins = new int[n];
+		int[] dp = new int[k+1];
+		
+		for (int i = 0; i < n; i++) {
+			coins[i] = sc.nextInt();
+		}
+		
+		dp[0] = 1;
+		
+		for (int j = 0; j < n; j++) {
+			for (int i = coins[j]; i <= k; i++) {
+				dp[i] += dp[i-coins[j]];
+			}
+		}
+		
+		System.out.println(dp[k]);
+		sc.close();
+	}
 }

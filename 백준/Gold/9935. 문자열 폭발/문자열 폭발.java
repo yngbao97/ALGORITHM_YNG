73aa507adc10bae66882e264054b426aa7ec87db
yngbao97 <156047141+yngbao97@users.noreply.github.com>
2024-08-22 10:11:15 +0900

    [Gold IV] Title: 문자열 폭발, Time: 400 ms, Memory: 81200 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		char[] input = br.readLine().toCharArray();
+		char[] bomb = br.readLine().toCharArray();
+		int N = bomb.length;
+		
+		int[] dp = new int[input.length];
+		Deque<Character> dq = new LinkedList<>();
+		
+		int bombIdx = 0;
+		int idx = 0;
+		
+		for (int i = 0; i < input.length; i++) {
+			
+			dq.addLast(input[i]);
+			
+//			System.out.println("----전----");
+//			System.out.println("idx: "+ idx);
+//			System.out.println("bombIdx: "+ bombIdx);
+			
+			if (input[i] == bomb[bombIdx]) dp[idx++] = bombIdx++;
+			else {
+				bombIdx = 0;
+				if (input[i] == bomb[bombIdx]) dp[idx++] = bombIdx++;
+				else dp[idx++] = -1;
+			}
+			
+			if (bombIdx == N) {
+//				System.out.println("----폭발 전----");
+//				System.out.println("idx: "+ idx);
+//				System.out.println("bombIdx: "+ bombIdx);
+				
+				for (int b = 0; b < N; b++) dq.pollLast();
+				idx -= N;
+				if (idx > 0) bombIdx = dp[idx-1] + 1;
+				else bombIdx = 0;
+				
+//				System.out.println("----폭발 후----");
+//				System.out.println("idx: "+ idx);
+//				System.out.println("bombIdx: "+ bombIdx);
+			}
+			
+//			System.out.println(Arrays.toString(dp));
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		if (dq.isEmpty()) sb.append("FRULA");
+		else while (!dq.isEmpty()) sb.append(dq.pollFirst());
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+}

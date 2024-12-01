06987e00a7376232038e42b738a0e93c3a0e982f
Tea._.pot <156047141+yngbao97@users.noreply.github.com>
2024-12-02 02:20:45 +0900

    [Silver II] Title: A → B, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -5,9 +5,6 @@ public class Main {
 	static BufferedReader br;
 	static BufferedWriter bw;
 	static StringTokenizer st;
-	static long A;
-	static long B;
-	static boolean[] visited;
 
 	public static void main(String[] args) throws Exception {
 
@@ -17,29 +14,26 @@ public class Main {
 
 		String[] input = br.readLine().split(" ");
 		
-		A = Long.parseLong(input[0]);
-		B = Long.parseLong(input[1]);
+		int A = Integer.parseInt(input[0]);
+		int B = Integer.parseInt(input[1]);
 		
-		visited = new boolean[(int) (B+1)];
+		int answer = 1;
 		
-		long answer = -1;
-		PriorityQueue<Node> qp = new PriorityQueue<>();
-		qp.add(new Node(A, 0));
-		
-		while (!qp.isEmpty()) {
+		while (A != B) {
 			
-			Node curr = qp.poll();
-			
-			if (curr.number == B) {
-				answer = curr.count + 1;
+			if (A > B) {
+				answer = -1;
 				break;
 			}
 			
-			long first = curr.number * 2;
-			long second = (curr.number * 10) + 1;
+			if (B % 2 == 0) B /= 2;
+			else if (B % 10 == 1) B /= 10;
+			else {
+				answer = -1;
+				break;
+			};
 			
-			if (first <= B) qp.add(new Node(first, curr.count + 1));
-			if (second <= B) qp.add(new Node(second, curr.count + 1));
+			answer++;
 		}
 		
 		bw.append(String.valueOf(answer));
@@ -48,23 +42,5 @@ public class Main {
 		bw.close();
 		br.close();
 
-	}
-}
-
-class Node implements Comparable<Node>{
-	long number;
-	long count;
-	
-	Node (long number, long count) {
-		this.number = number;
-		this.count = count;
-	}
-
-	@Override
-	public int compareTo(Node o) {
-		if (this.number == o.number) {
-			return Long.compare(this.count, o.count);
-		}
-		return Long.compare(this.number, number);
 	}
 }

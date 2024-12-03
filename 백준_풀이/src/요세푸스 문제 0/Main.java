9ada49db0fb39cf3b5dc90ac76fa7d8e8e3165fa
yngbao97 <yukyj0418@gmail.com>
2024-12-03 22:07:19 +0900

    문제 풀이 업로드

---
+++ 백준_풀이/src/요세푸스 문제 0/Main.java
@@ -0,0 +1,40 @@
+/**
+ * Author: yngbao97, Yuk Yejin
+ * Problem: 요세푸스 문제 0_11866
+ * Date: 2024.12.03
+ */
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while(!queue.isEmpty()) {
+            for (int i = 1; i < K; i++) {
+                queue.add(queue.poll());
+            }
+            int tmp = queue.poll();
+            if (queue.isEmpty()) sb.append(tmp+">");
+            else sb.append(tmp+", ");
+        }
+
+        System.out.println(sb.toString());
+        sc.close();
+    }
+}

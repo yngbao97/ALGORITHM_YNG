5779ead97f6c02681149742573fd5749bc858206
yngbao97 <yukyj0418@gmail.com>
2024-12-03 15:22:38 +0900

    Z_1074 문제 풀이

---
+++ 백준_풀이/src/N과 M (9)/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Author: yngbao97, Yuk Yejin
+ * Problem: N과 M (9)_15663
+ * Date: 2024.12.03
+ */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws Exception {
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
